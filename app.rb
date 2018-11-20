7ef815136976c1dfa9207071ad17cac24cd98e94
joaquimbarreto <joaquim@barreto.co>
2018-11-20 00:25:56 +0000

    going to sleep

--- app.rb
+++ app.rb
@@ -3,4 +3,28 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-end
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse!}!"
+  end
+
+  get '/square/:number' do
+    @number_sq = params[:number].to_i * params[:number].to_i
+    "#{@number_sq}!"
+  end
+
+  get '/say/:number/:phrase' do
+     (params[:number].to_i).times do
+       "#{params[:phrase]}" 
+     end
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}." 
+  end
+
+  # get '/:operation/:number1/:number2' do
+
+  # end
+
+end 
